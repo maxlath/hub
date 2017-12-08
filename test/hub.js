8bf436a40ad321891fd640c3801e1dbeb6f74f84
maxlath <k@maxlath.eu>
2017-12-08 22:23:33 +0100

    hub: guess the target language from the request headers

--- test/hub.js
+++ test/hub.js
@@ -3,15 +3,42 @@ const { port } = require('config')
 const host = 'http://localhost:' + port
 const breq = require('bluereq')
 
-const get = url => breq.get({ url: host+url, followRedirect: false })
+const get = (url, lang) => {
+  return breq.get({
+    url: host+url,
+    headers: { 'accept-language': lang },
+    followRedirect: false
+  })
+}
 
 describe('hub', () => {
-  it('should redirect to Wikipedia by default', done => {
+  it('should redirect to the English Wikipedia by default', done => {
     get('/Q184226')
     .then(res => {
       res.statusCode.should.equal(302)
       res.headers.location.should.equal('https://en.wikipedia.org/wiki/Gilles_Deleuze')
       done()
     })
+    .catch(done)
+  })
+
+  it('should guess the language from the headers', done => {
+    get('/Q184226', 'fr')
+    .then(res => {
+      res.statusCode.should.equal(302)
+      res.headers.location.should.equal('https://fr.wikipedia.org/wiki/Gilles_Deleuze')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should give priority to the language in the query', done => {
+    get('/Q184226?lang=de', 'fr')
+    .then(res => {
+      res.statusCode.should.equal(302)
+      res.headers.location.should.equal('https://de.wikipedia.org/wiki/Gilles_Deleuze')
+      done()
+    })
+    .catch(done)
   })
 })
