22813b2cbf5adabe33c1271eb6646290d52f734f
maxlath <k@maxlath.eu>
2017-12-14 00:13:25 +0100

    lint

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   // A function to quickly fail when a test gets an undesired positive answer
   undesiredRes: done => res => {
-    done(new Error(".then function was expected not to be called"))
+    done(new Error('.then function was expected not to be called'))
     logger.warn(res, 'undesired positive res')
   },
   undesiredErr: done => err => {
