6df679cfda667d18f83b60e2d12ddea12d7fcd1a
maxlath <k@maxlath.eu>
2017-12-28 23:40:20 +0100

    query: redirect correctly despite a key with spaces

--- test/query.js
+++ test/query.js
@@ -12,7 +12,7 @@ describe('query', () => {
     .catch(undesiredErr(done))
   })
 
-  it('should redirect to the hub separting params with escpaed spaces', done => {
+  it('should redirect to the hub separting params with escaped spaces', done => {
     get('/query?q=Q3%20s=wq%20l=es')
     .then(res => {
       res.statusCode.should.equal(302)
@@ -27,6 +27,16 @@ describe('query', () => {
     .then(res => {
       res.statusCode.should.equal(302)
       res.headers.location.should.equal('/Q3?s=wq&l=es')
+      done()
+    })
+    .catch(undesiredErr(done))
+  })
+
+  it('should redirect correctly despite a key with spaces', done => {
+    get('/query?q=fr:baden baden s=inv')
+    .then(res => {
+      res.statusCode.should.equal(302)
+      res.headers.location.should.equal('/fr:baden%20baden?s=inv')
       done()
     })
     .catch(undesiredErr(done))
