fcc1371b55ee842f178136a8e5bd50c888f5a1c7
maxlath <k@maxlath.eu>
2018-08-20 21:02:37 +0200

    links: accept a shortlist

--- test/links.js
+++ test/links.js
@@ -1,4 +1,4 @@
-require('should')
+const should = require('should')
 const { get } = require('./lib/utils')
 
 describe('links', function () {
@@ -8,10 +8,23 @@ describe('links', function () {
     get('/links/Q1972359')
     .then(res => {
       res.statusCode.should.equal(200)
-      console.log('body:', res.body)
       res.body.P214.should.be.an.Object()
       res.body.P214.url.should.equal('https://viaf.org/viaf/101675162')
       res.body.inventaire.url.should.equal('https://inventaire.io/entity/wd:Q1972359')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should return a shortlist when requested', done => {
+    get('/links/Q1972359?shortlist=inventaire|P214')
+    .then(res => {
+      res.statusCode.should.equal(200)
+      res.body.P214.should.be.an.Object()
+      res.body.P214.url.should.equal('https://viaf.org/viaf/101675162')
+      should(res.body.P227).not.be.ok()
+      res.body.inventaire.url.should.equal('https://inventaire.io/entity/wd:Q1972359')
+      should(res.body.scholia).not.be.ok()
       done()
     })
     .catch(done)
