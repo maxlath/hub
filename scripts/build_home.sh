d5e2ff7961313b893374dc6820eab7573ece49b7
maxlath <k@maxlath.eu>
2025-12-07 19:41:50 +0100

    scripts: fix env
    Using bash instead of sh to be able to use "echo -e"

--- scripts/build_home.sh
+++ scripts/build_home.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 marked ./README.md > ./assets/html/home.content.html
 ./scripts/assemble_home_parts.js > ./public/home.html
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 npm run update-properties
 npm run build
 
